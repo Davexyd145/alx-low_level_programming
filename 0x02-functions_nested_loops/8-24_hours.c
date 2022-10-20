25e4a41d9930a5bb7e4952449ffb6f6e3639e7a9
Davexyd145 <davisfxd145@gmail.com>
2022-10-19 19:26:06 -0700

    Task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -3,19 +3,20 @@
 /**
  * jack_bauer - prints every minute of the day
  */
-void jacj_bauer(void)
+void jack_bauer(void)
 {
 	int hour, minute;
 
-	for (hour = 0; hour <= 23; hour++)
-
+	for (hour = 0; hour <= 24; hour++)
+	{
 		for (minute = 0; minute <= 59; minute++)
 		{
-			_putchar((hour / 10) + '0');
-			_putchar((hour % 10) + '0');
-			_putchar(':');
-			_putchar((minute / 10) + '0');
-		        _putchar((minute % 10) + '0');
+			_putchar((hour / 100 + '0');
+			_putchar((hour / 100 + '0');
+			_putchar((hour % 100 + '0');
+			_putchar(':')
+			_putchar((minute / 100 + '0');
 			_putchar('\n');
 		}
-}	
+	}
+}
