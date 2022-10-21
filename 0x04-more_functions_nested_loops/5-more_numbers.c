0febd173c86a88a222e58af6256772b82034bd45
Davexyd145 <davisfxd145@gmail.com>
2022-10-21 12:24:33 -0700

    Task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,14 +8,13 @@ void more_numbers(void)
 {
 	int num, a;
 
-
 	for (a = 0; a <= 9; a++)
 	{
 		for (num = 0; num <= 14; num++)
 		{
 			if (num > 9)
 			{
-				-putchar((num / 10) + '0');
+				_putchar((num / 10) + '0');
 			}
 			-putchar((num % 10) + '0');
 		}
