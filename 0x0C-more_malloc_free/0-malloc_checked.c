8c6ab71bd7b1ee040800d27d5979a32ca2b52fb9
Davexyd145 <davisfxd145@gmail.com>
2022-11-11 05:42:08 -0800

    Task 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,11 +1,11 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
  * malloc_checked - array for prints a string
  * @b: number of memory
  * Return: void
  */
+
 void *malloc_checked(unsigned int b)
 {
 void *p;
